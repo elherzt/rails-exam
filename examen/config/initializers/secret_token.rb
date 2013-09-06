042b3a9a4a31edc02acd76f2abf32dc99ca1cb02
Jesus Romero <jose.romero@tangosource.com>
2013-09-06 01:21:02 -0500

    created rails application

---
+++ examen/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Examen::Application.config.secret_key_base = '908aa1d8346e9b1fdd201fc90b73a7ee6224a77d8b245cd582aeb9d4293ffb791d568c45493536ff5ea5194b6da19b6ab96fe0ee854bb3aaf313c2c0ddb46929'
---
+++ examen/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Examen::Application.config.session_store :cookie_store, key: '_examen_session'
